c2a621342e5a8a9791edc6325f3af41b380b446c
ValuhMarko <marko.valuh@gmail.com>
2026-02-15 17:43:37 +0100

    prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/DeFi-Saver-UI-Engineer-Challenge/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
